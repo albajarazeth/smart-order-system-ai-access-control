e859b726d3e2d5900d6ad240c7201a951d26aa1f
Mariano-Gongora <mariano.gongora55@gmail.com>
2025-05-03 17:49:08 -0500

    [Revert]

--- orders-system-be/src/main/java/com/smart_order_system/orders_system_be/EmailBot/EmailBot.java
+++ orders-system-be/src/main/java/com/smart_order_system/orders_system_be/EmailBot/EmailBot.java
@@ -1,17 +1,5 @@
 package com.smart_order_system.orders_system_be.EmailBot;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
-
 public class EmailBot {
-//     ChatModel chatModel="gemini-2.0-flash";
-
-//     public void emailBot(String email){
-//         String response=ChatClient.create(null)
-//         .prompt("Whats up")
-//         .call()
-//         .content();
-
-//         System.out.println(response);
-// }
+    
 }
